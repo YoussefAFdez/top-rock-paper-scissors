81c2054930e232fab52568f6864e717c228fb683
Youssef Addouli Fernandez <yuu@youssefs-macbook-pro.home>
2023-10-04 20:20:30 +0200

    Comment code
    Add javadoc style comments and in-line comments

--- script.js
+++ script.js
@@ -1,16 +1,25 @@
 document.addEventListener("DOMContentLoaded", main);
 
 function main() {
+
   console.log(playRound(prompt("Choose your action:"), getComputerChoice()));
 }
 
+/**
+ * Evaluates the player choice and computer's and returns the result of the match
+ * @param {*} playerChoice Choice asked from the player
+ * @param {*} computerChoice Choice generated randomly
+ * @returns The result of the game
+ */
 function playRound(playerChoice, computerChoice) {
+  //Transform values to lower case for better comparison
   playerChoice = playerChoice.toLowerCase();
   computerChoice = computerChoice.toLowerCase();
 
   //Loose: 0, Win: 1, Draw: 2
   let result = "";
 
+  //We evaluate the game, and provide a result value (0 equals to loose, 1 equals to win, 2 equals to draw)
   switch (playerChoice) {
     case "rock":
       if (computerChoice === "rock") result = 2;
@@ -28,6 +37,7 @@ function playRound(playerChoice, computerChoice) {
       else result = 2;
   }
 
+  //We evaluate the result, returning a value in each case
   switch (result) {
     case 0:
       return `You loose! ${computerChoice} beats ${playerChoice}!`;
@@ -40,6 +50,10 @@ function playRound(playerChoice, computerChoice) {
   }
 }
 
+/**
+ * Generates a number between 0 and 2, and returns a value of an array in this index.
+ * @returns Random value between "Rock", "Paper", "Scissors"
+ */
 function getComputerChoice() {
   let choices = ["Rock", "Paper", "Scissors"];
   let choice = Math.floor(Math.random() * 3);
